abf15e24b7f8867bf483d9def6daffbce872be3e
Felipe Chandía Castillo <felipe@MacBook-Pro-de-Felipe-2.local>
2023-12-27 15:46:33 -0300

    mails2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //const serverUrl = 'http://localhost:3003/';
-//const serverUrl = 'http://192.168.0.121:3003/'
-const serverUrl = 'http://192.168.0.110:3003/'
+const serverUrl = 'http://192.168.0.121:3003/'
+//const serverUrl = 'http://192.168.0.110:3003/'
 
 export { serverUrl }
